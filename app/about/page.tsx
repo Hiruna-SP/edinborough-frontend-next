0e943653f3948d38e1ddff12ea3fda823122f6cb
Hiruna SP <hirunasp@gmail.com>
2026-08-04 22:25:04 +0530

    Refactor StatsBar component: move to common directory, enhance props for customization

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,15 @@
 import HeroBanner from "@/components/common/heroBanner";
-import StatsBar from "@/components/home/statsBar";
+import StatsBar from "@/components/common/statsBar";
 
 export default function About() {
   return (
     <main className="min-h-screen bg-white">
-      <HeroBanner 
+      <HeroBanner
         title="About Edinborough"
         text="From a home kitchen in 1973 to one of Sri Lanka's leading food companies, our journey has always been driven by quality, innovation, and trust."
         backgroundImage="/images/about/herobanner.png"
       />
-      <StatsBar
-        bgColor="#F3F3F3"
-        textColor="#000000"
-        dividerColor="#FF0000"
-      />
+      <StatsBar bgColor="#F3F3F3" textColor="#000000" dividerColor="#FF0000" />
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HomeHero from "@/components/home/homeHero";
 import ProductDiscoverySection from "@/components/home/ProductDiscoverySection";
 import ProductsBanner from "@/components/home/productsBanner";
 import SoyaSauceHero from "@/components/home/soyaSauseBanner";
-import StatsBar from "@/components/home/statsBar";
+import StatsBar from "@/components/common/statsBar";
 import TonightRecipes from "@/components/home/tonightSection";
 
 export default function Home() {
